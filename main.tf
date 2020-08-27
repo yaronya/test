d43f3a11b63220e774d93a2d469b105288d6a7c2
Yaron Yarimi <yaron.yarimi@env0.com>
2020-08-27 11:50:03 +0300

    pingdom

--- main.tf
+++ main.tf
@@ -3,3 +3,17 @@ resource "null_resource" "null" {
 
 resource "null_resource" "null2" {
 }
+
+resource "pingdom_team" "test_one" {
+  name = "Team 1 (updated by Terraform)"
+}
+
+resource "pingdom_user" "first_user" {
+  username = "johndoe"
+}
+
+resource "pingdom_contact" "first_user_contact_email_1" {
+  user_id        = "${pingdom_user.first_user.id}"
+  email          = "john@doe.com"
+  severity_level = "HIGH"
+}
