42d51109ef0dd2c942bdf31cdad339af05c52015
Yaron Yarimi <yaron.yarimi@env0.com>
2020-08-27 14:13:56 +0300

    test

--- main.tf
+++ main.tf
@@ -1,19 +1,39 @@
-resource "null_resource" "null" {
+provider "pingdom" {
+    user = "test"
+    password = "test"
+    api_key = "test"
+    account_email = "test" # Optional: only required for multi-user accounts
 }
 
-resource "null_resource" "null2" {
+resource "pingdom_check" "example" {
+    type = "http"
+    name = "my http check"
+    host = "example.com"
+    resolution = 5
 }
 
-resource "pingdom_team" "test_one" {
-  name = "Team 1 (updated by Terraform)"
+resource "pingdom_check" "example_with_alert" {
+    type = "http"
+    name = "my http check"
+    host = "example.com"
+    resolution = 5
+    sendnotificationwhendown = 2
+    integrationids = [
+      12345678,
+      23456789
+    ]
+    userids = [
+      24680,
+      13579
+    ]
 }
 
-resource "pingdom_user" "first_user" {
-  username = "johndoe"
-}
-
-resource "pingdom_contact" "first_user_contact_email_1" {
-  user_id        = "${pingdom_user.first_user.id}"
-  email          = "john@doe.com"
-  severity_level = "HIGH"
+resource "pingdom_check" "ping_example" {
+    type = "ping"
+    name = "my ping check"
+    host = "example.com"
+    resolution = 1
+    userids = [
+      24680
+    ]
 }
