375aa374b9892563976fc025565da3003bb516e8
Yaron Yarimi <yaron.yarimi@env0.com>
2020-11-23 18:55:37 +0200

    test

--- null-template/main.tf
+++ null-template/main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+terraform {
+  required_version = ">= 0.12"
+}
+
 resource "aws_s3_bucket" "test" {
   bucket = "env0-s3-${random_string.random.result}"
   acl    = "public-read"
