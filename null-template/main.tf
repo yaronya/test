78e75c4cf5be1b2392c3fe9f5bd4317103ea2ff8
Yaron Yarimi <yaron.yarimi@env0.com>
2020-11-23 18:46:31 +0200

    test

--- null-template/main.tf
+++ null-template/main.tf
@@ -1,9 +1,10 @@
-terraform {
-  required_version = "0.12.26"
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
 }
 
-resource "null_resource" "null" {
-}
-
-resource "null_resource" "null2" {
+resource "aws_s3_bucket" "test" {
+  bucket = "env0-s3-${random_string.random.result}"
+  acl    = "public-read"
+  force_destroy = true
 }
