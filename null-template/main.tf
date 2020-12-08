9af6e58fff37feb06b464bd79b7f8b7edb8829aa
Yaron Yarimi <yaronya@users.noreply.github.com>
2020-12-08 17:18:00 +0200

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -1,14 +1,9 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = "us-east-1"
-}
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = "0.12.26"
 }
 
-resource "aws_s3_bucket" "test" {
-  bucket = "env0-s3-test-bucket-123"
-  acl    = "public-read"
-  force_destroy = true
+resource "null_resource" "null" {
+}
+
+resource "null_resource" "null2" {
 }
