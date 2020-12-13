9c8dd4f4e257b49f6cb5f4ab71acac0d79d3496a
Yaron Yarimi <yaronya@users.noreply.github.com>
2020-12-13 13:35:18 +0200

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -12,6 +12,6 @@ resource "random_string" "random" {
 
 resource "aws_s3_bucket" "yaron" {
   bucket = "yaron-test-${random_string.random.result}"
-  acl    = "bla"
+  acl    = "public-read"
   force_destroy = true
 }
