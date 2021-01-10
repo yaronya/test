1ac29c739859e83cebea4dc2f3e9ec65ba3b76fa
Yaron Yarimi <yaronya@users.noreply.github.com>
2021-01-10 20:46:40 +0200

    null

--- null-template/main.tf
+++ null-template/main.tf
@@ -1,19 +1,9 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = "us-east-1"
+terraform {
+  required_version = "0.12.26"
 }
 
-resource "aws_instance" "test" {
-  ami = "ami-0c654c3ab463d22f6"
-  instance_type = "t2.micro"
+resource "null_resource" "null" {
 }
 
-resource "aws_instance" "test2" {
-  ami = "ami-0c654c3ab463d22f6"
-  instance_type = "t2.micro"
-}
-
-resource "aws_instance" "test3" {
-  ami = "ami-0c654c3ab463d22f6"
-  instance_type = "t2.micro"
+resource "null_resource" "null2" {
 }
