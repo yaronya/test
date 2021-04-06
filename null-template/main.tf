fd3a12e813196d7a1864133a251dabbbe407a351
Yaron Yarimi <yaronya@users.noreply.github.com>
2021-04-06 14:24:22 +0300

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -2,6 +2,24 @@ terraform {
   required_version = "0.12.26"
 }
 
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+
+resource "random_string" "random" {
+  length = "16"
+  special = "false"
+  min_lower = "16"
+}
+
+
+resource "aws_s3_bucket" "yaron" {
+  bucket = "yaron-test-${random_string.random.result}"
+  force_destroy = true
+}
+
 resource "null_resource" "null" {
 }
 
