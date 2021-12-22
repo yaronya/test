30b8da3b3a35b591b1c9bd2d109cbf2247d51b42
Yaron Yarimi <yaronya@users.noreply.github.com>
2021-12-22 10:55:09 +0200

    Update main.tf

--- private-tf-submodules/main.tf
+++ private-tf-submodules/main.tf
@@ -1,8 +1,3 @@
-module "private" {
-  source = "api-dev.dev.env0.com/78937575-9722-4543-98bb-c14890cff69f/private/yaronya"
-  version = "0.0.2"
-}
-
 module "another" {
   source = "api-dev.dev.env0.com/78937575-9722-4543-98bb-c14890cff69f/another/yaronya"
   version = "0.0.2"
