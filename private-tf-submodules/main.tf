35eadc24df10d9ed8baa2891bc49fb98588ce09b
Yaron Yarimi <yaronya@users.noreply.github.com>
2022-02-07 17:16:49 +0200

    Update main.tf

--- private-tf-submodules/main.tf
+++ private-tf-submodules/main.tf
@@ -1,6 +1,5 @@
-module "private-module" {
-  source = "api-dev.dev.env0.com/78937575-9722-4543-98bb-c14890cff69f/private-module/yaryona"
-  version = "0.0.2"
+module "private-module2" {
+  source = "git@github.com:env0/env0.git"
 }
 
 resource "null_resource" "null" {
