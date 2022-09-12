7f4fd89aa8fefdd4e095b6fee0f8ba783e8c70f3
Yaron Yarimi <yaronya@users.noreply.github.com>
2022-09-12 17:43:53 +0300

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -14,7 +14,7 @@ resource "random_string" "random" {
   min_lower = "16"
 }
 
-resource "aws_s3_bucket" "yaron" {
+resource "aws_s3_bucket" "yaron2" {
   bucket = "yaron-tessst-${random_string.random.result}"
   force_destroy = true
 }
