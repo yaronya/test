5055f5d8d51094ee1ad4eac76fc0eb929a85eb7d
Yaron Yarimi <yaronya@users.noreply.github.com>
2023-12-11 15:51:12 +0200

    x

--- aws/module/main.tf
+++ aws/module/main.tf
@@ -1,15 +1,5 @@
-terraform {
-  required_providers {
-    aws        = "~> 3.70.0"
-  }
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
-
 resource "aws_elasticache_cluster" "example" {
-  cluster_id           = "yaron-test-cluster-3"
+  cluster_id           = "yaron-test-cluster-4"
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
--- aws/terragrunt.hcl
+++ aws/terragrunt.hcl
@@ -1,3 +1,13 @@
 terraform {
-  source = "../module"
+  source = "./module"
+}
+
+generate "provider" {
+  path = "provider.tf"
+  if_exists = "overwrite_terragrunt"
+  contents = <<EOF
+provider "aws" {
+  region = "us-east-1"
+}
+EOF
 }
