f3f4f1772ab6dbf477e273ccee38cebafeb1ce4b
Yaron Yarimi <yaronya@users.noreply.github.com>
2023-12-11 14:29:22 +0200

    Update main.tf

--- aws/module/main.tf
+++ aws/module/main.tf
@@ -1,5 +1,5 @@
 resource "aws_elasticache_cluster" "example" {
-  cluster_id           = "yaron-test-cluster"
+  cluster_id           = "yaron-test-cluster-2"
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
