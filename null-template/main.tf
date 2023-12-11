1c3b8ca1d1d0f0c7ef364618c347f9e6eb4f845d
Yaron Yarimi <yaronya@users.noreply.github.com>
2023-12-11 10:55:26 +0200

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -1,20 +1,9 @@
-terraform {
-  required_providers {
-    aws        = "~> 3.70.0"
-  }
+resource "null_resource" "null" {
 }
 
-provider "aws" {
-  region  = "us-east-1"
+resource "null_resource" "null2234" {
 }
 
-resource "random_string" "random" {
-  length = "16"
-  special = "false"
-  min_lower = "16"
-}
-
-resource "aws_s3_bucket" "yaron244443" {
-  bucket = "yaron-tessst-${random_string.random.result}"
-  force_destroy = true
+output "yaron" {
+  value = "blabla"
 }
