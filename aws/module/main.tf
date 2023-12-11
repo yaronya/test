9c2e28ef9e800dba34a7d8f8233fe5c267dacc1c
Yaron Yarimi <yaronya@users.noreply.github.com>
2023-12-11 14:16:57 +0200

    Update main.tf

--- aws/module/main.tf
+++ aws/module/main.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "example" {
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis5.0"
   engine_version       = "5.0.6"
   port                 = 6379
 }
